4fb13d715bfb8d5ec4d812afd559628450d91777
avinesh culloo <avineshculloo@gmail.com>
2021-10-26 22:13:49 +0400

    Revert the changes back from using php in index.php

--- index.php
+++ index.php
@@ -97,9 +97,41 @@
                 </div>
             </div>
         </div>
+    </body>
+    <footer >
+        <!-- Grid container -->
+        <div class="container p-4 pb-0">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+                <!-- Facebook -->
+                <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/keshav.culloo" target="_blank" role="button">
+                    <i class="fa fa-facebook icon"></i>
+                </a>
 
-<?php
-    include('common/common.php');
-    generateFooter();
+                <!-- Instagram -->
+                <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/keshav.culloo/" target="_blank" role="button">
+                    <i class="fa fa-instagram icon"></i>
+                </a>
 
-?>
+                <!-- Linkedin -->
+                <a class="btn btn-outline-light btn-floating m-1" href="https://www.linkedin.com/in/culloo-avinesh-46b3b71b6/" target="_blank" role="button">
+                    <i class="fa fa-linkedin icon"></i>
+                </a>
+
+                <!-- Github  -->
+                <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/avinesh1906" target="_blank" role="button">
+                    <i class="fa fa-github icon"></i>
+                </a>
+            </section>
+            <!-- Section: Social media -->
+
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2021 Copyright: Avinesh Culloo
+        </div>
+        <!-- Copyright -->
+    </footer>
+</html>
