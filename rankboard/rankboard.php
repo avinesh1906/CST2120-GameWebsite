141de0a451f06d1003accd2dbc14bec83326bcf6
avinesh culloo <avineshculloo@gmail.com>
2021-10-28 22:49:19 +0400

    Shift rankboard.html to rankboard.php

---
+++ rankboard/rankboard.php
@@ -0,0 +1,42 @@
+<?php
+    $pageName = 'Log In';
+    $folderName = 'login';
+    include('../common/common.php');
+    generateHeader($pageName, $folderName);
+    generateNavBar($pageName);
+?>
+            <div class="rankboard_header">
+                <h2> Leaderboard </h2>
+            </div>
+            <div class="rankboard_table">
+                <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Rank</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Avinesh Culloo</td>
+                        <td>54</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Sounil Gopee</td>
+                        <td>54</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Anoopama Boyjoo</td>
+                        <td> 49</td>
+                    </tr>
+                    
+                </tbody>
+                </table>
+            </div>
+<?php
+    generateFooter();
+?>
