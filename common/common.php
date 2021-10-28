7cd9f892db970a87bf049c60254f6e7e456d0cc4
avinesh culloo <avineshculloo@gmail.com>
2021-10-28 12:00:15 +0400

    Fixed logical error (location for css

--- common/common.php
+++ common/common.php
@@ -25,7 +25,7 @@ echo '<!-- Required meta tags -->';
         
         if ($title == 'Home') {
             echo '<!-- External CSS -->';
-            echo '<link href="./css/styles.css" type="text/css" rel="stylesheet">';
+            echo '<link href="./home/css/styles.css" type="text/css" rel="stylesheet">';
             echo '<!-- Bootstrap JS -->';
             echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>';
         }
