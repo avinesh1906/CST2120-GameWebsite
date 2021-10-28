8d18c612afcfb76b83c729ab5cc1321433785d02
Avinesh Culloo <avineshculloo@gmail.com>
2021-10-28 15:46:56 +0400

    Realign the index.php file

--- index.php
+++ index.php
@@ -7,44 +7,43 @@
     generateNavBar($pageName);
 ?>
 
-            <div class="playgame">
-                <div class="playgame_header">
-                    <h3> Choose your difficulty level: </h3>
-                </div>
-                <form class="options">
-                    <input type="radio" id="beginner" name="game_option" value="beginner" checked="checked"> 
-                    <label for="beginner">Beginner </label><br>
-                    <input type="radio" id="normal" name="game_option" value="normal"> 
-                    <label for="normal">Normal </label><br>
-                    <input type="radio" id="time-attack" name="game_option" value="time-attack"> 
-                    <label for="time-attack">Time Attack </label>
-                </form>
-                <div class="playgame_footer">
-                    <div class="playgame_btn">
-                        <button type="submit">Play Game</button>
-                    </div>
-                    <div class="playgame_option">
-                        <a href="../howtoplay/howtoplay.html">How to Play?</a>
-                    </div>
-                </div>
+<div class="playgame">
+    <div class="playgame_header">
+        <h3> Choose your difficulty level: </h3>
+    </div>
+    <form class="options">
+        <input type="radio" id="beginner" name="game_option" value="beginner" checked="checked"> 
+        <label for="beginner">Beginner </label><br>
+        <input type="radio" id="normal" name="game_option" value="normal"> 
+        <label for="normal">Normal </label><br>
+        <input type="radio" id="time-attack" name="game_option" value="time-attack"> 
+        <label for="time-attack">Time Attack </label>
+    </form>
+    <div class="playgame_footer">
+        <div class="playgame_btn">
+            <button type="submit">Play Game</button>
+        </div>
+        <div class="playgame_option">
+            <a href="../howtoplay/howtoplay.html">How to Play?</a>
+        </div>
+    </div>
+</div>
+<!-- Carousel -->
+<div class="carousel-container">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img class="d-block" src="./common/img/js-snake.png" width="450"  alt="First slide">
             </div>
-            <!-- Carousel -->
-            <div class="carousel-container">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                        <img class="d-block" src="./common/img/js-snake.png" width="450"  alt="First slide">
-                        </div>
-                        <div class="carousel-item">
-                        <img class="d-block" src="./common/img/apple-snake.png"  width="450" alt="Second slide">
-                        </div>
-                        <div class="carousel-item">
-                        <img class="d-block" src="./common/img/snakeitup.png"  width="450"  alt="Third slide">
-                        </div>
-                    </div>
-                </div>
+            <div class="carousel-item">
+            <img class="d-block" src="./common/img/apple-snake.png"  width="450" alt="Second slide">
             </div>
-
+            <div class="carousel-item">
+            <img class="d-block" src="./common/img/snakeitup.png"  width="450"  alt="Third slide">
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
     generateFooter();
