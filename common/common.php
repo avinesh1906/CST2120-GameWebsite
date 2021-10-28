819f61dd97044b71f68d215b835ac8abcead2c9d
avinesh culloo <avineshculloo@gmail.com>
2021-10-28 13:50:21 +0400

    Add code.jquery

--- common/common.php
+++ common/common.php
@@ -25,7 +25,7 @@ echo '<!-- Required meta tags -->';
             echo '<!-- External'. $directoryname. 'CSS -->';
             echo '<link href="./'. $directoryname .'/css/styles.css" type="text/css" rel="stylesheet">';
             echo '<!-- Bootstrap JS -->';
-            echo '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>';
+            echo '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>';
         } else {
             echo '<!-- Tab icon-->';
             echo '<link href="../common/img/AC.png" rel="icon">';
