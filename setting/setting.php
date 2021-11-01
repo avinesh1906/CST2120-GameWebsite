c7863e508de97912fbc1b5b8f447bad510537632
Avinesh Culloo <avineshculloo@gmail.com>
2021-11-01 12:30:33 +0400

    Modify the css for setting.php

--- setting/setting.php
+++ setting/setting.php
@@ -5,35 +5,35 @@
     generateHeader($pageName, $folderName);
     generateNavBar($pageName);
 ?>
-            <div class="setting_header">
-                <h2> Change Password </h2>
-            </div>
-            <div class="setting_form">
-                <div class="form_details">
-                    <label for="current_password" class="form-label"> Currrent </label>
-                    <div class="form-input">
-                        <input autocomplete="off" type="password" id="current_password">
-                    </div>
-                </div>
-                <div class="form_details">
-                    <label for="new_password" class="form-label"> New </label>
-                    <div class="form-input">
-                        <input autocomplete="off" type="password" id="new_password">
-                    </div>
-                </div>
-                <div class="form_details">
-                    <label for="confirm_password" class="form-label"> Retype New </label>
-                    <div class="form-input">
-                        <input autocomplete="off" type="password" id="confirm_password">
-                    </div>
-                </div>
-                <div class="form_details">
-                    <h5> It's a good idea to use a strong password that you don't use elsewhere. </h5>
-                </div>
-            </div>
-            <div class="setting_footer">
-                <button type="submit">Save Changes</button>
-            </div>
+<div class="setting_header">
+    <h2> Change Password </h2>
+</div>
+<div class="setting_form">
+    <div class="form_details">
+        <label for="current_password" class="form-label"> Currrent </label>
+        <div class="form-input">
+            <input autocomplete="off" type="password" id="current_password">
+        </div>
+    </div>
+    <div class="form_details">
+        <label for="new_password" class="form-label"> New </label>
+        <div class="form-input">
+            <input autocomplete="off" type="password" id="new_password">
+        </div>
+    </div>
+    <div class="form_details">
+        <label for="confirm_password" class="form-label"> Retype New </label>
+        <div class="form-input">
+            <input autocomplete="off" type="password" id="confirm_password">
+        </div>
+    </div>
+    <div class="form_details">
+        <h5> It's a good idea to use a strong password that you don't use elsewhere. </h5>
+    </div>
+</div>
+<div class="setting_footer">
+    <button type="submit">Save Changes</button>
+</div>
 <?php
     generateFooter($pageName);
 ?>
