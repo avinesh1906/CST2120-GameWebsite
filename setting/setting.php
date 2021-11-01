18e9112eed511c66d9033f01194fa3cb0e1cc5e7
Avinesh Culloo <avineshculloo@gmail.com>
2021-11-01 20:54:14 +0400

    Add comments and insert form plus submit

--- setting/setting.php
+++ setting/setting.php
@@ -1,47 +1,69 @@
 <?php
+    // php variables
     $pageName = 'Setting';
     $folderName = 'setting';
+
+    // include the common.php which contains the php functions
     include('../common/common.php');
+
+    // functions call
     generateHeader($pageName, $folderName);
     generateNavBar($pageName);
 ?>
+
+<!-- Setting Page Title -->
 <div class="setting_header">
     <h2> Change Password </h2>
 </div>
+
+<!-- Setting form -->
 <div class="setting_form">
+    <!-- Left side Gif -->
     <div class="left_side_picture">
         <img class="side_picture" src="../common/img/serpent.gif" width="250" alt="side_picture" >
     </div>
-    <div class="main_form">
-        <div class="form_details">
-            <label for="current_password" class="form-label"> Currrent </label>
-            <div class="form-input">
-                <input autocomplete="off" type="password" id="current_password">
+    <!-- Setting form -->
+    <form class="form-layout" action="../index.php">
+        <!-- Form Label and Input -->
+        <div class="form-content">
+            <!-- Current Password -->
+            <div class="form_details">
+                <label for="current_password" class="form-label"> Currrent </label>
+                <div class="form-input">
+                    <input autocomplete="off" type="password" id="current_password">
+                </div>
+            </div>
+            <!-- New Password -->
+            <div class="form_details">
+                <label for="new_password" class="form-label"> New </label>
+                <div class="form-input">
+                    <input autocomplete="off" type="password" id="new_password">
+                </div>
+            </div>
+            <!-- Confirm New Password -->
+            <div class="form_details">
+                <label for="confirm_password" class="form-label"> Retype New </label>
+                <div class="form-input">
+                    <input autocomplete="off" type="password" id="confirm_password">
+                </div>
+            </div>
+            <!-- Form text -->
+            <div class="form_details">
+                <h5> It's a good idea to use a strong password that you don't use elsewhere. </h5>
             </div>
         </div>
-        <div class="form_details">
-            <label for="new_password" class="form-label"> New </label>
-            <div class="form-input">
-                <input autocomplete="off" type="password" id="new_password">
-            </div>
+        <div class="setting_footer">
+            <button type="submit">Save Changes</button>
         </div>
-        <div class="form_details">
-            <label for="confirm_password" class="form-label"> Retype New </label>
-            <div class="form-input">
-                <input autocomplete="off" type="password" id="confirm_password">
-            </div>
-        </div>
-        <div class="form_details">
-            <h5> It's a good idea to use a strong password that you don't use elsewhere. </h5>
-        </div>
-    </div>
+    </form>
+    <!-- Right side Gif -->
     <div class="right_side_picture">
-    <img class="side_picture" src="../common/img/serpent.gif"  width="250" alt="side_picture" >
+        <img class="side_picture" src="../common/img/serpent.gif"  width="250" alt="side_picture" >
     </div>
 </div>
-<div class="setting_footer">
-    <button type="submit">Save Changes</button>
-</div>
+
+<!-- Php function to generate footer -->
 <?php
+    // Function call
     generateFooter($pageName);
 ?>
