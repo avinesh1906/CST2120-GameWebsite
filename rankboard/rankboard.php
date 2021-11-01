84c86d5fa8463a82731bf5977fd93c8193feb559
Avinesh Culloo <avineshculloo@gmail.com>
2021-11-01 20:58:01 +0400

    Added comments in rankboard.php

--- rankboard/rankboard.php
+++ rankboard/rankboard.php
@@ -1,44 +1,56 @@
 <?php
+    // php variables
     $pageName = 'Rankboard';
     $folderName = 'rankboard';
+
+    // include the common.php which contains the php functions
     include('../common/common.php');
+
+    // function calls
     generateHeader($pageName, $folderName);
     generateNavBar($pageName);
 ?>
 
+<!-- Rankboard page Title -->
 <div class="rankboard_header">
     <h2> Leaderboard </h2>
 </div>
+
+<!-- Rankboard table -->
 <div class="rankboard_table">
     <table class="table table-hover">
-    <thead>
-        <tr>
-            <th scope="col">Rank</th>
-            <th scope="col">Name</th>
-            <th scope="col">Score</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <th scope="row">1</th>
-            <td>Avinesh Culloo</td>
-            <td>54</td>
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>Sounil Gopee</td>
-            <td>54</td>
-        </tr>
-        <tr>
-            <th scope="row">3</th>
-            <td>Anoopama Boyjoo</td>
-            <td> 49</td>
-        </tr>
-        
-    </tbody>
+        <!-- Table Head --> 
+        <thead>
+            <tr>
+                <th scope="col">Rank</th>
+                <th scope="col">Name</th>
+                <th scope="col">Score</th>
+            </tr>
+        </thead>
+        <!-- Table Body -->
+        <tbody>
+            <!-- Table Rows -->
+            <tr>
+                <th scope="row">1</th>
+                <td>Avinesh Culloo</td>
+                <td>54</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Sounil Gopee</td>
+                <td>54</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Anoopama Boyjoo</td>
+                <td> 49</td>
+            </tr>
+        </tbody>
     </table>
 </div>
 
+<!-- Php function to generate footer -->
 <?php
+    // function call
     generateFooter($pageName);
 ?>
