b5e37965efb000c72ccd15b0e21cbb66b76af309
Avinesh Culloo <avineshculloo@gmail.com>
2021-11-19 23:09:11 +0400

    Commented common.js

--- common/js/common.js
+++ common/js/common.js
@@ -1,9 +1,11 @@
+// call function init when window loads
 window.onload = init;
 
+//  function init
 function init() {
-    //Create array of users if it does not exist.
+    // Create array of users if it does not exist.
     if(localStorage.users === undefined){
-        //Existing users
+        //Existing users (objects as arrays)
         let users = [
             {
                 username: "avinesh1906",
@@ -30,23 +32,30 @@ function init() {
                 score: 49
             }
         ];
+        // convert the JS object array into JSON
         let usrDetails = JSON.stringify(users);
+        // create the local storage for storing user details
         localStorage.users = usrDetails;
     };
 
 }
 
+//  variable to store the toggleEye details
 let toggle_name = document.getElementById("toggleEye");
+
+// variable to store the URL content
 let pathArray = window.location.pathname.split('/');
+// access the last element of pathname
 let pathname = pathArray[pathArray.length - 1];
+// check if pathname is setting.php or register.php
 if (pathname == 'setting.php' || pathname == 'register.php' ){
     // event listener to detect click on the eye
     toggle_name.addEventListener("click", togglePassword);
 }
 
+//  function for togglePaassword
 function togglePassword() {
     // variables
-    
     let pwd = document.getElementById(toggle_name.getAttribute("name"));
     let type = pwd.getAttribute("type");
 
