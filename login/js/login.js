eb126d8f7324c492daeb5b5b50bb47d831027ddf
Avinesh Culloo <avineshculloo@gmail.com>
2021-11-19 23:37:07 +0400

    Commented login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,14 +1,17 @@
+// global variables
 let btn = document.getElementById("submit_btn");
 let usr = document.getElementById("Username");
 let pwd = document.getElementById("Password");
 
+// call the function validateLoginForm when submit button is clicked
 btn.onclick = validateLoginForm;
 
-
+// function to validate username
 function usernameValidation() {
     // variables 
     let details = document.getElementById("usr_details");
 
+    // check if input field is empty
     if (usr.value.length == 0) {
         details.innerHTML = '*required';
         details.style.color = "#FDD2BF";
@@ -16,16 +19,19 @@ function usernameValidation() {
         return false;
     } 
 
+    // succes message
     btn.disabled = false;
     details.innerHTML = "";
     return true;
     
 }
 
+// function to validate password
 function passwordValidation() {
-
+    // local variable
     let details = document.getElementById("pwd_details");
 
+    // check if input field is empty
     if (pwd.value.length == 0) {
         details.innerHTML = '*required';
         details.style.color = "#FDD2BF";
@@ -36,10 +42,13 @@ function passwordValidation() {
     return true;
 }
 
+// function to validate login form in general
 function validateLoginForm(){
+    // convert JSON into js objects
     let users = JSON.parse(localStorage.users);
     let details = document.getElementById("pwd_details");
 
+    // call all functions to check if meet all requirements
     if (!usernameValidation() && !passwordValidation()){
         return false;
     } else {
@@ -49,10 +58,14 @@ function validateLoginForm(){
         if (!passwordValidation()){
             return false;
         }
+        // loop through the user objects
         for (i = 0; i < users.length; i++){
+            //  check if password corresponds to that particular username
             if (users[i].username == usr.value && users[i].password == pwd.value){
                 btn.disabled = false;
+                // redirect to home page
                 window.location.href="../index.php";
+            // error messages
             } else {
                 details.innerHTML = '*username/password incorrect';
                 details.style.color = "#FDD2BF";
