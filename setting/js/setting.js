fb24fa6b45ffb55262c8a7f84869d8a452b438bf
Avinesh Culloo <avineshculloo@gmail.com>
2021-11-19 23:18:27 +0400

    Commented setting.js

--- setting/js/setting.js
+++ setting/js/setting.js
@@ -1,28 +1,37 @@
+// global variables
 let btn = document.getElementById("submit_btn");
 
+// call function init on window load
 window.onload = init;
 
+// function init
 function init() {
+    // disable submit button
     btn.disabled = true;
 }
 
+// function to validate old password
 function oldPassword() {
     // variables 
     let details = document.getElementById("currentPWD_details");
     let pwd = document.getElementById("current_password");
 
+    //  check if the input field is empty
     if (pwd.value.length == 0) {
+        // error message
         details.innerHTML = '*required';
         details.style.color = "#FDD2BF";
         btn.disabled = true;
         return false;
     }
+    // success message
     btn.disabled = false;
     details.innerHTML = "";
     return true;
     
 }
 
+//  function to validate new password
 function passwordValidation() {
     // variables 
     let details = document.getElementById("newPWD_details");
@@ -36,19 +45,23 @@ function passwordValidation() {
     let old_pwd = document.getElementById("current_password");
     let pwd = document.getElementById("new_password");
 
+    //  check if input field is empty
     if (pwd.value.length == 0) {
         details.innerHTML = '*required';
         details.style.color = "#FDD2BF";
         return false;
+    // check if new password passes the regex test
     } else if (!re.test(pwd.value)) {
         details.innerHTML = " Password should contain at least one symbol, <br> upper and lower case letter <br> and a number of min 6 characters. </span>";
         details.style.color = "#FDD2BF";
         return false;
+    // check if new password is same as the old password
     } else if (pwd.value == old_pwd.value) {
         details.innerHTML = 'New password cannot be the same <br> as your old password.';
         details.style.color = "#FDD2BF";
         return false;
     } 
+    // sucessful message
     btn.disabled = false;
     details.innerHTML = "Strong Password";
     details.style.color = "#77D970";
@@ -56,6 +69,7 @@ function passwordValidation() {
     
 }
 
+// function to verify confirm password matches new password
 function confirmPassword() {
     //variables
     let button = document.getElementById("submit_btn");
@@ -63,24 +77,27 @@ function confirmPassword() {
     let pwd = document.getElementById("new_password");
     let confirm_pwd = document.getElementById("confirm_password");
 
+    // check for empty input field
     if (confirm_pwd.value.length == 0) {
         details.innerHTML = '*required';
         details.style.color = "#FDD2BF";
         return false;
+    // check if password matches
     } else if (confirm_pwd.value != pwd.value) {
         details.innerHTML = "password does not match";
         details.style.color = "#FDD2BF";
         return false;
     }
+    // sucessful messages
     btn.disabled = false;
     details.innerHTML = "Password matches ";
     details.style.color = "#77D970";
     return true;
-    
-
 }
 
+// function to validate setting form in general
 function validateSettingForm() {
+    let users = JSON.parse(localStorage.users);
     if (!oldPassword() && !passwordValidation() && !confirmPassword()){
         return false;
         
@@ -94,6 +111,10 @@ function validateSettingForm() {
         if (!confirmPassword()){
             return false;
         }
+
+        // for (i = 0; i < users.length; i++){
+
+        // };
         btn.disabled = false;
         return true;
     } 
