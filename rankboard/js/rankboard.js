4b6c3ab7255929d695a83c9d294b53ea2af0f1cf
Avinesh Culloo <avineshculloo@gmail.com>
2021-11-19 23:04:36 +0400

    Added comments in rankboard.js

--- rankboard/js/rankboard.js
+++ rankboard/js/rankboard.js
@@ -1,34 +1,32 @@
-
-console.log("uhmm");
-
-
-
-generateTable();
-
-function sortScore(){
-
-}
-
+// function to generate the content of the table body
 function generateTable(){
+    // retrieve only the tbody tag name from the table
     let table = document.getElementById("myTable").getElementsByTagName('tbody')[0];
-    console.log(table);
+
+    // convert JSON(html local storage data) into JS objects
     let users = JSON.parse(localStorage.users);
+    
+    // Sort the JS objects into descending orders
     users.sort((a,b) =>
     {
         return b.score - a.score;
     });
 
+    //  loop through the JS objects
     for (i = 0; i < users.length; i++){
+        // create variable for row
         let row = table.insertRow(i);
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
-        let cell3 = row.insertCell(2);
+        // create variable for cells
+        let rank = row.insertCell(0);
+        let username = row.insertCell(1);
+        let score = row.insertCell(2);
 
-        cell1.innerHTML = i+1;
-        cell2.innerHTML = users[i].username;
-        cell3.innerHTML = users[i].score;
+        // assign the content to the cells
+        rank.innerHTML = i+1;
+        username.innerHTML = users[i].username;
+        score.innerHTML = users[i].score;
     }
-
 }
 
-
+//  call the function generateTable()
+generateTable();
