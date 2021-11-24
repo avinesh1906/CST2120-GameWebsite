477c9ac59c5dd0f01e3c3baa5a12ef5811432724
Avinesh Culloo <avineshculloo@gmail.com>
2021-11-24 16:13:40 +0400

    Created the verify login function

--- game/js/game.js
+++ game/js/game.js
@@ -1,16 +1,19 @@
-
+// call the function verify Login
 verifyLogin();
 
-
+// function verifyLogin 
 function verifyLogin() 
 {   
+    // variables
     let text_message = document.getElementById("notLoggedIn");
     let game_window = document.getElementsByClassName("game-window")[0];
-    console.log(game_window);
+
+    // check if session storage is empty
     if (sessionStorage.loggedUser == undefined){
+        // hide the div text_message
         text_message.style.display = "block";
     } else {
-        console.log(game_window);
+        // display the div text_message
         game_window.style.display = "block";
     }
 }
